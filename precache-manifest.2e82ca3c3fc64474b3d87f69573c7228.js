a9c5cdfc27e41f4ef77f97695acaf63198fea933
bq-hentai <18297927925@163.com>
2018-12-18 09:38:08 +0800

    add core static files

---
+++ precache-manifest.2e82ca3c3fc64474b3d87f69573c7228.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c9432cf5b416a6446f3",
+    "url": "/css/app.612dd383.css"
+  },
+  {
+    "revision": "7c9432cf5b416a6446f3",
+    "url": "/js/app.0d00e2c6.js"
+  },
+  {
+    "revision": "099dcc36d6b9bc4532d1",
+    "url": "/css/chunk-vendors.77f64a73.css"
+  },
+  {
+    "revision": "099dcc36d6b9bc4532d1",
+    "url": "/js/chunk-vendors.fa5158c6.js"
+  },
+  {
+    "revision": "a467680b917e2a144baa",
+    "url": "/css/login.9011fbe3.css"
+  },
+  {
+    "revision": "a467680b917e2a144baa",
+    "url": "/js/login.9840ec73.js"
+  },
+  {
+    "revision": "1b35b521c0082d56b9ac413ddb0660ef",
+    "url": "/fonts/font_148784_v4ggb6wrjmkotj4i.1b35b521.woff"
+  },
+  {
+    "revision": "62759b1cf27452e085eddbb8ab29ad2b",
+    "url": "/fonts/font_148784_v4ggb6wrjmkotj4i.62759b1c.eot"
+  },
+  {
+    "revision": "92acc6829cf612e62ecf2be54e9344f0",
+    "url": "/fonts/font_148784_v4ggb6wrjmkotj4i.92acc682.ttf"
+  },
+  {
+    "revision": "f3572d89e4b28ee4b2d49d560a95e7eb",
+    "url": "/img/500.f3572d89.png"
+  },
+  {
+    "revision": "71a5fc392f50d54ebfb23c7e85458a37",
+    "url": "/img/404.71a5fc39.png"
+  },
+  {
+    "revision": "e5bfd0a8109a578756959a4907499d95",
+    "url": "/img/403.e5bfd0a8.png"
+  },
+  {
+    "revision": "ca5d4588dad9c32f49e895e7f19479df",
+    "url": "/img/font_148784_v4ggb6wrjmkotj4i.ca5d4588.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "33d44f7efaaffed75238127c006cd7a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1a40cfdaf3d977aadc80239d141652e",
+    "url": "/login.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
